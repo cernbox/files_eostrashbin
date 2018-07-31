6db79bda461d7806ff1ecb4e858794de9e0079d0
Hugo Gonzalez Labrador <github@hugo.labkode.com>
2018-07-31 16:49:08 +0200

    disable detail view

--- js/app.js
+++ js/app.js
@@ -18,22 +18,15 @@ OCA.EosTrashbin.App = {
 			$('#app-content-eostrashbin'), {
 				scrollContainer: $('#app-content'),
 				fileActions: this._createFileActions(),
-				detailsViewEnabled: true,
+				detailsViewEnabled: false,
 				scrollTo: urlParams.scrollto,
-				//config: OCA.Files.App.getFilesConfig()
+				config: OCA.Files.App.getFilesConfig()
 			}
 		);
 	},
 
 	_createFileActions: function() {
 		var fileActions = new OCA.Files.FileActions();
-		fileActions.register('dir', 'Open', OC.PERMISSION_READ, '', function (filename, context) {
-			var dir = context.fileList.getCurrentDirectory();
-			context.fileList.changeDirectory(OC.joinPaths(dir, filename));
-		});
-
-		fileActions.setDefault('dir', 'Open');
-
 		fileActions.registerAction({
 			name: 'Restore',
 			displayName: t('files_eostrashbin', 'Restore'),
@@ -55,37 +48,6 @@ OCA.EosTrashbin.App = {
 				);
 			}
 		});
-
-		/* EOS does not support single file purging so we disable it
-		fileActions.registerAction({
-			name: 'Delete',
-			displayName: t('files', 'Delete'),
-			mime: 'all',
-			permissions: OC.PERMISSION_READ,
-			iconClass: 'icon-delete',
-			render: function(actionSpec, isDefault, context) {
-				var $actionLink = fileActions._makeActionLink(actionSpec, context);
-				$actionLink.attr('original-title', t('files_eostrashbin', 'Delete permanently'));
-				$actionLink.children('img').attr('alt', t('files_eostrashbin', 'Delete permanently'));
-				context.$file.find('td:last').append($actionLink);
-				return $actionLink;
-			},
-			actionHandler: function(filename, context) {
-				var fileList = context.fileList;
-				$('.tipsy').remove();
-				var tr = fileList.findFileEl(filename);
-				var deleteAction = tr.children("td.date").children(".action.delete");
-				deleteAction.removeClass('icon-delete').addClass('icon-loading-small');
-				fileList.disableActions();
-				$.post(OC.filePath('files_eostrashbin', 'ajax', 'delete.php'), {
-						files: JSON.stringify([filename]),
-						dir: fileList.getCurrentDirectory()
-					},
-					_.bind(fileList._removeCallback, fileList)
-				);
-			}
-		});
-		*/
 
 		return fileActions;
 	}
